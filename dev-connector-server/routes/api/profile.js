3e43adbe0a33351a3f711b377683c58fe1a6140e
Harsh Jha <harshjhaa11@gmail.com>
2020-08-14 18:01:50 +0530

    Added github repo to profile route

--- dev-connector-server/routes/api/profile.js
+++ dev-connector-server/routes/api/profile.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const { check, validationResult } = require('express-validator');
-
+const request = require('request')
+const config = require('config')
 
 const auth = require('../../middleware/auth')
 const Profile = require('../../models/Profile')
@@ -294,5 +295,27 @@ router
             res.status(500).send("Server Error");
         }
     })
-    
+    //@route        GET api/profile/github/:username
+    //@description  Getting github profile
+    //@access       Public
+    .get('/github/:username', async (req, res) => {
+        try {
+            const options = {
+                uri: `https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc&client_id=${config.get('githubClientId')}&client_secret=${config.get('githubSecret')}`,
+                method: 'GET',
+                headers: { 'user-agent': 'node.js' }
+            };
+            request(options, (error, response, body) => {
+                if (error)
+                    console.log(error);
+                if (response.statusCode !== 200)
+                    return res.status(404).json({ msg: "No Github Profile Found" })
+                return res.json(JSON.parse(body));
+            })
+        } catch (err) {
+            console.log(err.message);
+            res.status(500).send("Server Error");
+        }
+    })
+
 module.exports = router;
